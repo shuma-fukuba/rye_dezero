64bef27e3b3b708fffa47dca0f000d7c7c9fc181
fukuba-shuma <calculus.fukuba@gmail.com>
2023-08-27 22:25:46 +0900

    gradリセット実装;

--- src/dezero/__init__.py
+++ src/dezero/__init__.py
@@ -14,6 +14,9 @@ class Variable:
     def set_creator(self, func):
         self.creator = func
 
+    def clear_grad(self):
+        self.grad = None
+
     def backward(self):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
@@ -21,60 +24,84 @@ class Variable:
         funcs = [self.creator]
         while funcs:
             f = funcs.pop()
-            x, y = f.x, f.output
-            x.grad = f.backward(y.grad)
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
 
-            if x.creator is not None:
-                funcs.append(x.creator)
+                if x.creator is not None:
+                    funcs.append(x.creator)
 
 
 class Function:
     def __init__(self) -> None:
-        self.x: Variable = None
-        self.output: Variable = None
+        self.inputs = None
+        self.outputs = None
 
-    def __call__(self, val: Variable):
-        x = val.data
-        y = self.forward(x)
-        output = Variable(as_array(y))
-        output.set_creator(self)
-        self.x = val
-        self.output = output
-        return output
+    def __call__(self, *inputs: Variable):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs if len(outputs) > 1 else outputs[0]
 
     def forward(self, x):
         raise NotImplementedError()
 
     def backward(self, gy):
         raise NotImplementedError()
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        return x0 + x1
+
+    def backward(self, gy):
+        return gy, gy
 
 
 class Square(Function):
     def forward(self, x):
-        return x ** 2
+        return x**2
 
     def backward(self, gy):
-        x = self.x.data
+        x = self.inputs[0].data
         gx = 2 * x * gy
         return gx
 
 
-class Exp(Function):
-    def forward(self, x):
-        return np.exp(x)
+# class Exp(Function):
+#     def forward(self, x):
+#         return np.exp(x)
 
-    def backward(self, gy):
-        x = self.x.data
-        gx = np.exp(x) * gy
-        return gx
+#     def backward(self, gy):
+#         x = self.inputs.data
+#         gx = np.exp(x) * gy
+#         return gx
 
 
 def square(x: np.ndarray):
     return Square()(x)
 
 
-def exp(x: np.ndarray):
-    return Exp()(x)
+# def exp(x: np.ndarray):
+#     return Exp()(x)
+
+
+def add(x0, x1):
+    return Add()(x0, x1)
 
 
 def numerical_diff(f, x, eps=1e-4):
@@ -89,3 +116,15 @@ def as_array(x) -> np.ndarray:
     if np.isscalar(x):
         return np.array(x)
     return x
+
+
+if __name__ == "__main__":
+    x = Variable(np.array(3.0))
+    y = add(x, x)
+    y.backward()
+    print(x.grad)
+
+    x.clear_grad()
+    y = add(add(x, x), x)
+    y.backward()
+    print(x.grad)
