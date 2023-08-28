bfa6f855f20b102c19777ab92e168be058d64601
fukuba-shuma <calculus.fukuba@gmail.com>
2023-08-28 22:27:11 +0900

    step 27

--- src/steps/step27.py
+++ src/steps/step27.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 from dezero import Function
@@ -19,9 +21,20 @@ def sin(x: Variable) -> Variable:
     return Sin()(x)
 
 
+def my_sin(x: Variable, threshold=0.0001):
+    y = 0
+    for i in range(100000):
+        c = (-1) ** i / math.factorial(2 * i + 1)
+        t = c * x ** (2 * i + 1)
+        y = y + t
+        if abs(t.data) < threshold:
+            break
+    return y
+
+
 if __name__ == "__main__":
     x = Variable(np.array(np.pi / 4))
-    y = sin(x)
+    y = my_sin(x)
     y.backward()
 
     print(y.data)
